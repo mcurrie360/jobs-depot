e94bdf16a5b52c5cc3ad90456ca6f29ad2ed2bdf
Mike <mcurrie360@gmail.com>
2019-06-26 13:13:47 -0400

    Add files via upload

---
+++ jobsdepot/templates/inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+        <p>&copy; 2019 Jobs Depot, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+ </body>
+
+</html>
---
+++ jobsdepot/templates/inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jobs Depot</title>
+	<link rel="stylesheet" href="http://bootswatch.com/4/flatly/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="index.php">Home</a></li>
+            <li role="presentation"><a href="create.php">Create Listing</a></li>
+            <!--<li role="presentation"><a href="#">Contact</a></li>-->
+          </ul>
+        </nav>
+        <h3 class="text-muted">Jobs Depot<?php //echo SITE_TITLE; ?></h3>
+      </div>
+      <?php displayMessage(); ?>
+
+
+<!--</body>
+</html>-->
